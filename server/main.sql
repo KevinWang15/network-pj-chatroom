80e3f45514c771792d0cd77425bb6edb005112fd
Kevin Wang <453587854@qq.com>
2016-12-02 01:33:55 +0800

    清空数据库

--- server/main.sql
+++ server/main.sql
@@ -2,21 +2,36 @@
 Navicat SQLite Data Transfer
 
 Source Server         : chatroom
-Source Server Version : 20817
+Source Server Version : 30802
 Source Host           : :0
 
 Target Server Type    : SQLite
-Target Server Version : 20817
+Target Server Version : 30802
 File Encoding         : 65001
 
-Date: 2016-11-30 14:53:47
+Date: 2016-12-02 01:33:33
 */
 
 PRAGMA foreign_keys = OFF;
 
 -- ----------------------------
+-- Table structure for chat_history
+-- ----------------------------
+DROP TABLE IF EXISTS "main"."chat_history";
+CREATE TABLE "chat_history" (
+"id"  INTEGER NOT NULL,
+"user_id"  INTEGER,
+"target_id"  INTEGER,
+"target_type"  TEXT,
+"data"  BLOB,
+"sent"  INTEGER,
+PRIMARY KEY ("id" ASC)
+);
+
+-- ----------------------------
 -- Table structure for friends
 -- ----------------------------
+DROP TABLE IF EXISTS "main"."friends";
 CREATE TABLE "friends" (
 "from_user_id"  INTEGER NOT NULL,
 "to_user_id"  INTEGER NOT NULL,
@@ -27,6 +42,7 @@ PRIMARY KEY ("from_user_id" ASC, "to_user_id")
 -- ----------------------------
 -- Table structure for rooms
 -- ----------------------------
+DROP TABLE IF EXISTS "main"."rooms";
 CREATE TABLE "rooms" (
 "id"  INTEGER NOT NULL,
 "room_name"  TEXT,
@@ -36,6 +52,7 @@ PRIMARY KEY ("id")
 -- ----------------------------
 -- Table structure for room_user
 -- ----------------------------
+DROP TABLE IF EXISTS "main"."room_user";
 CREATE TABLE "room_user" (
 "id"  INTEGER NOT NULL,
 "room_id"  INTEGER,
@@ -46,6 +63,7 @@ PRIMARY KEY ("id")
 -- ----------------------------
 -- Table structure for users
 -- ----------------------------
+DROP TABLE IF EXISTS "main"."users";
 CREATE TABLE "users" (
 "id"  INTEGER NOT NULL,
 "username"  TEXT,
